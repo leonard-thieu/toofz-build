fcb71a770cccbe1d8ab69200f04ad3c8e6942de9
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-18 12:30:25 -0400

    Assume OpenCover is installed globally.

--- Test-Project.ps1
+++ Test-Project.ps1
@@ -1,15 +1,13 @@
 if (-not(Test-Path Env:\PROJECT)) { throw 'The environment variable "PROJECT" is not set. Tests will not be run.' }
+
 $project = $env:PROJECT
 $configuration = $env:CONFIGURATION
 if ($configuration -eq $null) { $configuration = 'Debug' }
 
-[xml]$xml = Get-Content "$project.Tests\packages.config"
-$version = ($xml.packages.package | ? { $_.id -eq 'OpenCover' }).version
-
 $targetArgs = ".\$project.Tests\bin\$configuration\$project.Tests.dll"
 if (Test-Path Env:\APPVEYOR) { $targetArgs += ' /logger:AppVeyor' }
 
-& "packages\OpenCover.$version\tools\OpenCover.Console.exe" `
+& "OpenCover.Console.exe" `
     "-register:user" `
     "-target:vstest.console.exe" `
     "-targetargs:$targetArgs" `
