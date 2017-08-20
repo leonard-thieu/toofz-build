47f1b12bd5f3b1092e40ec9dbc84e90fc7724fd9
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-20 13:58:09 -0400

    Allow additional filters to be added.

--- Test-Project.ps1
+++ Test-Project.ps1
@@ -1,3 +1,8 @@
+[CmdletBinding()]
+param(
+    [String]$Filter
+)
+
 if (-not(Test-Path Env:\PROJECT)) { throw 'The environment variable "PROJECT" is not set. Tests will not be run.' }
 
 $project = $env:PROJECT
@@ -7,11 +12,14 @@ if ($configuration -eq $null) { $configuration = 'Debug' }
 $targetArgs = ".\$project.Tests\bin\$configuration\$project.Tests.dll"
 if (Test-Path Env:\APPVEYOR) { $targetArgs += ' /logger:AppVeyor' }
 
+$filter = "-filter:+[$project*]* -[$project.Tests*]*";
+if ($Filter -ne $null) { $filter += " $Filter" }
+
 OpenCover.Console.exe `
     "-register:user" `
     "-target:vstest.console.exe" `
     "-targetargs:$targetArgs" `
     "-returntargetcode" `
-    "-filter:+[$project*]* -[$project.Tests*]*" `
+    "$filter" `
     "-excludebyattribute:*.ExcludeFromCodeCoverage*;*.GeneratedCodeAttribute*"
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
