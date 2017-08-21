760c966a397cac81237fb3e00dce03c1ed48e3b0
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-21 11:14:59 -0400

    Wrong variable.

--- Submit-CodeCoverageResults.ps1
+++ Submit-CodeCoverageResults.ps1
@@ -5,7 +5,7 @@ param(
 
 if ($Project -eq '') { throw 'The environment variable "PROJECT" or the parameter "Project" is not set. Code coverage results have not been submitted.' }
 
-[Xml]$packagesConfig = Get-Content "$project.Tests\packages.config"
+[Xml]$packagesConfig = Get-Content "$Project.Tests\packages.config"
 $version = ($packagesConfig.packages.package | ? { $_.id -eq 'Codecov' }).version
 
 & "packages\Codecov.$version\tools\codecov.exe --file results.xml"
