a10c1733ba83a7cbfaf8fe4d775a701923bddbb0
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-23 12:15:28 -0400

    Enable pushing NuGet packages and NuGet symbol packages to MyGet.

--- Deploy-NuGetPackage.ps1
+++ Deploy-NuGetPackage.ps1
@@ -1,31 +1,40 @@
-$appveyor_repo_tag = $env:APPVEYOR_REPO_TAG
+[CmdletBinding()]
+param(
+    [String]$RepoTag = $env:APPVEYOR_REPO_TAG,
+    [String]$Configuration = $env:CONFIGURATION,
+    [String]$Platform = $env:PLATFORM,
+    [String]$Project = $env:PROJECT,
+    [String]$MyGetApiKey = $env:MYGET_API_KEY
+)
 
-if ($appveyor_repo_tag -ne 'true') {
-    Write-Warning ('The environment variable "APPVEYOR_REPO_TAG" is not set to "true". ' +
+if ($RepoTag -ne 'true') {
+    Write-Warning ('The environment variable "APPVEYOR_REPO_TAG" or the parameter "-RepoTag" is not set to "true". ' +
                    'NuGet package has not been deployed.')
 } else {
-    $configuration = $env:CONFIGURATION
-    $platform = $env:PLATFORM
-    $appveyor_build_folder = $env:APPVEYOR_BUILD_FOLDER
-    $project = $env:PROJECT
+    if ($Configuration -eq $null) { $Configuration = 'Debug' }
 
-    if ($configuration -eq $null) { $configuration = 'Debug' }
-
-    switch ($platform) {
+    switch ($Platform) {
         'x86' { break }
         'x64' { break }
-        default { $platform = 'AnyCPU' }
+        default { $Platform = 'AnyCPU' }
     }
 
-    nuget pack "$appveyor_build_folder\$project\$project.csproj" `
-        -Properties "Configuration=$configuration;Platform=$platform" `
-        -Verbosity quiet `
-        -NonInteractive
+    nuget pack "$Project\$Project.csproj" `
+        -Properties "Configuration=$Configuration;Platform=$Platform" `
+        -Symbols `
+        -Verbosity quiet
     if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-    [xml]$nuspec = Get-Content -Path "$appveyor_build_folder\$project\$project.nuspec"
+    [Xml]$nuspec = Get-Content -Path "$Project\$Project.nuspec"
     $id = $nuspec.package.metadata.id
     $version = $nuspec.package.metadata.version
     
-    Push-AppveyorArtifact "$appveyor_build_folder\$id.$version.nupkg"
+    nuget push $id.$version.nupkg `
+        -Source https://www.myget.org/F/toofz/api/v2/package -ApiKey $MyGetApiKey `
+        -SymbolSource https://www.myget.org/F/toofz/symbols/api/v2/package -SymbolApiKey $MyGetApiKey `
+        -Verbosity quiet
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+    Push-AppveyorArtifact "$id.$version.nupkg"
+    Push-AppveyorArtifact "$id.$version.symbols.nupkg"
 }
