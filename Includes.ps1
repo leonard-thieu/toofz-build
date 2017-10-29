21879f2bbd57ea0d32bc8c6514eaea862e38f64e
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-29 18:18:22 -0400

    Fix path.

--- Includes.ps1
+++ Includes.ps1
@@ -4,32 +4,24 @@ function Get-Project($projectName) {
     return $project
 }
 
-function Get-TargetFramework($project) {
-    $targetFramework = $project.Project.PropertyGroup.TargetFramework
+function Is-FrameworkVersion($version) {
+    return $targetFramework.StartsWith('v')
+}
 
-    # .NET Framework projects
-    if ($targetFramework.StartsWith('v')) {
-        return '.NET Framework'
-    }
-    # .NET Standard/Core projects
-    else {
-        return '.NET Core'
-    }
+function Get-TargetFramework($project) {
+    return $project.Project.PropertyGroup.TargetFramework
 }
 
 function Get-PackageVersion($projectName, $packageName) {
     $project = Get-Project $projectName
     $targetFramework = Get-TargetFramework $project
 
-    switch ($targetFramework) {
-        '.NET Framework' {
-            [Xml]$packagesConfig = Get-Content ".\$projectName\packages.config"
+    if (Is-FrameworkVersion $targetFramework) {
+        [Xml]$packagesConfig = Get-Content ".\$projectName\packages.config"
         
-            return ($packagesConfig.packages.package | ? { $_.id -eq $packageName }).version   
-        }
-        '.NET Core' {
-            return ($project.Project.ItemGroup.PackageReference | ? { $_.Include -eq $packageName }).Version
-        }
+        return ($packagesConfig.packages.package | ? { $_.id -eq $packageName }).version
+    } else {
+        return ($project.Project.ItemGroup.PackageReference | ? { $_.Include -eq $packageName }).Version
     }
 }
 
@@ -41,9 +33,10 @@ function Get-PackagePath($projectName, $packageName) {
     $project = Get-Project $projectName
     $targetFramework = Get-TargetFramework $project
 
-    switch ($targetFramework) {
-        '.NET Framework' { $packageDirectory = "$packageName.$version" }
-        '.NET Core' { $packageDirectory = $packageName.ToLower() + "\$version" }
+    if (Is-FrameworkVersion $targetFramework) {
+        $packageDirectory = "$packageName.$version"
+    } else {
+        $packageDirectory = $packageName.ToLower() + "\$version"
     }
     
     return Resolve-Path ".\packages\$packageDirectory"
@@ -53,8 +46,9 @@ function Get-OutputPath($projectName) {
     $project = Get-Project $projectName
     $targetFramework = Get-TargetFramework $project
 
-    switch ($targetFramework) {
-        '.NET Framework' { return Resolve-Path ".\$projectName\bin\$configuration\$projectName.dll" }
-        '.NET Core' { return Resolve-Path ".\$projectName\bin\$configuration\$targetFramework\$projectName.dll" }
+    if (Is-FrameworkVersion $targetFramework) {
+        return Resolve-Path ".\$projectName\bin\$configuration\$projectName.dll"
+    } else {
+        return Resolve-Path ".\$projectName\bin\$configuration\$targetFramework\$projectName.dll"
     }
 }
