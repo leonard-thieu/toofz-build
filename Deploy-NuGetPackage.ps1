02b64b307c08707f569dbc834c23272467ed65b7
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-30 12:15:37 -0400

    .

--- Deploy-NuGetPackage.ps1
+++ Deploy-NuGetPackage.ps1
@@ -40,6 +40,9 @@ if ($Env:APPVEYOR_REPO_TAG -ne 'true') {
     } else {
         msbuild /t:pack "/property:Configuration=$Configuration;Platform=$Platform;IncludeSymbols=true;IncludeSource=true" $projectPath
 
+        $id = $Project
+        $version = $projectObj.Version
+
         $outDir = [IO.Path]::Combine($projectDir, 'bin', $Configuration) | Resolve-Path
         $package = Join-Path $outDir ".\$id.$version.nupkg" | Resolve-Path
         $symbols = Join-Path $outDir ".\$id.$version.symbols.nupkg" | Resolve-Path
