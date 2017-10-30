68bc6b69cac24ef74e7087fe3d3b2780f8fe40d4
Leonard Thieu <leonard.thieu@gmail.com>
2017-10-30 11:09:10 -0400

    Allow relative paths.

--- toofz.Build/GetProjectCmdlet.cs
+++ toofz.Build/GetProjectCmdlet.cs
@@ -11,7 +11,17 @@ namespace toofz.Build
             Position = 0,
             ValueFromPipeline = true
         )]
-        public string Path { get; set; }
+        public string Path
+        {
+            get => path;
+            set
+            {
+                path = System.IO.Path.IsPathRooted(value) ?
+                    value :
+                    System.IO.Path.Combine(CurrentProviderLocation("FileSystem").ProviderPath, value);
+            }
+        }
+        private string path;
 
         protected override void ProcessRecord()
         {
