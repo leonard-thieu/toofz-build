c99fbb571b418db807b188a14a04dcec39d920d4
Leonard Thieu <leonard.thieu@gmail.com>
2017-12-02 13:23:15 -0500

    .

--- Link-Source.ps1
+++ Link-Source.ps1
@@ -1,12 +1,23 @@
-$Project = $env:PROJECT
-$Configuration = $env:CONFIGURATION
+[CmdletBinding()]
+param(
+    [String]$Project = $env:PROJECT,
+    [String]$Configuration = $env:CONFIGURATION,
+    [String]$SolutionDir = $env:APPVEYOR_BUILD_FOLDER,
+    [String]$RepoName = $env:APPVEYOR_REPO_NAME
+)
 
-nuget install SourceLink -ExcludeVersion -SolutionDirectory . -Verbosity quiet
+if ($Project -eq '') { throw 'The environment variable "PROJECT" or the parameter "Project" is not set. Tests have not been run.' }
+if ($Configuration -eq '') { $Configuration = 'Debug' }
+if ($SolutionDir -eq '') { $SolutionDir = Resolve-Path . }
+if ($RepoName -eq '') { throw 'The environment variable "APPVEYOR_REPO_NAME" or the parameter "RepoName" is not set. Source linking has not run.' }
+
+nuget install SourceLink -ExcludeVersion -SolutionDirectory $SolutionDir -Verbosity quiet
 $sourceLink = Resolve-Path ".\packages\SourceLink\tools\SourceLink.exe"
 Write-Debug "SourceLink path = $sourceLink"
 
 & $sourceLink index `
     --proj ".\$Project\$Project.csproj" `
     --proj-prop Configuration $Configuration `
-    --url "https://raw.githubusercontent.com/$env:APPVEYOR_REPO_NAME/{0}/%var2%"
+    --url "https://raw.githubusercontent.com/$RepoName/{0}/%var2%" `
+    --repo $SolutionDir
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
