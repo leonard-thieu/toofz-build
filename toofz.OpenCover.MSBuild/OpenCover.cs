27cf99b40c1427444eb34c0d89e9066f3803ebd5
Leonard Thieu <leonard.thieu@gmail.com>
2017-12-25 19:01:45 -0500

    feat: Convert Test-Project to MSBuild

---
+++ toofz.OpenCover.MSBuild/OpenCover.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Utilities;
+
+namespace toofz.OpenCover.MSBuild
+{
+    public class OpenCover : global::OpenCover.MSBuild.OpenCover
+    {
+        public bool OldStyle { get; set; }
+
+        protected override string GenerateCommandLineCommands()
+        {
+            var builder = new CommandLineBuilder();
+
+            if (OldStyle) { builder.AppendSwitch("-oldstyle"); }
+
+            return string.Join(" ", base.GenerateCommandLineCommands(), builder.ToString());
+        }
+    }
+}
