0e3fe36e5a298e7f7ed181fa40a8dbde0048fa41
Leonard Thieu <leonard.thieu@gmail.com>
2017-12-26 00:38:54 -0500

    feat: Convert Deploy-NuGetPackage to MSBuild

---
+++ toofz.Build.Tasks/PushPackage.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace toofz.Build.Tasks
+{
+    public class PushPackage : ToolTask
+    {
+        [Required]
+        public ITaskItem Package { get; set; }
+        public string Source { get; set; }
+        public string ApiKey { get; set; }
+        public string SymbolSource { get; set; }
+        public string SymbolApiKey { get; set; }
+
+        protected override string ToolName => "NuGet.exe";
+
+        protected override string GenerateFullPathToTool()
+        {
+            var path = ToolPath;
+            var exe = Path.GetFileName(ToolExe);
+
+            return Path.GetFullPath(Path.Combine(path, exe));
+        }
+
+        protected override string GenerateCommandLineCommands()
+        {
+            var builder = new CommandLineBuilder();
+            builder.AppendSwitch("push");
+
+            builder.AppendFileNameIfNotNull(Package);
+            builder.AppendSwitchIfNotNull("-Source ", Source);
+            builder.AppendSwitchIfNotNull("-ApiKey ", ApiKey);
+            builder.AppendSwitchIfNotNull("-SymbolSource ", SymbolSource);
+            builder.AppendSwitchIfNotNull("-SymbolApiKey ", SymbolApiKey);
+
+            return builder.ToString();
+        }
+    }
+}
